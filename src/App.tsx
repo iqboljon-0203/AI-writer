74486750b796e62b071e60c74436367e766baebc
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2024-07-17 10:52:51 +0500

    Added all files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { Button } from "./components/ui/button";
+function App() {
+    return (
+        <>
+           <h1 className='text-red-700 underline'>Salom</h1>
+            <Button>Click me</Button>
+        </>
+    )
+}
+
+export default App
